fe22e1b0564e9f85168dd1de7dd4ad16e5550ddb
orzorzooo <orzorzooo@gmail.com>
2023-05-26 16:56:11 +0800

    EDIT product create vue EDIT product update vue

---
+++ src/views/products/update.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <eform :product="product" v-if="product"></eform>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import { get, post } from "@/api/request";
+import eform from "./components/form.vue";
+const props = defineProps(["id"]);
+const product = ref(null);
+
+async function init() {
+  const { data } = await get({ collection: `products/${props.id}` });
+  console.log("product", data);
+  product.value = data;
+}
+init();
+</script>
