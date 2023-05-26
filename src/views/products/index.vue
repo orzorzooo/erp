4be7758c4f01e4b85a48b3a03d68a8a2fe82e8c6
orzorzooo <orzorzooo@gmail.com>
2023-05-26 16:58:21 +0800

    EDIT product index 樣式

--- src/views/products/index.vue
+++ src/views/products/index.vue
@@ -1,18 +1,14 @@
 <template>
   <div>
-    <!-- switch parent border: <el-switch v-model="parentBorder" /> switch child
-    border: <el-switch v-model="childBorder" /> -->
     <pure-table :data="products" :columns="columns" stripe v-loading="loading">
       <template #expand="{ row }">
         <div class="m-4">
           <p>{{ row.description }}</p>
         </div>
         <div class="p-4">
-          <el-button type="primary" @click="edit">Edit</el-button>
-          <el-button>Cancel</el-button>
+          <el-button type="primary" @click="edit(row.id)">編輯商品</el-button>
         </div>
       </template>
-      <!-- <template #description="{ row }"></template> -->
     </pure-table>
   </div>
 </template>
@@ -20,10 +16,12 @@
 import { get } from "@/api/request";
 import { ref } from "vue";
 import { PureTable } from "@pureadmin/table";
+import { useRouter } from "vue-router";
 const products = ref([]);
 const parentBorder = ref(false);
 const childBorder = ref(false);
 const loading = ref(true);
+const router = useRouter();
 const columns = [
   {
     type: "expand",
@@ -32,14 +30,14 @@ const columns = [
   { label: "名稱", prop: "name" },
   { label: "進貨價", prop: "purchase_price" },
   { label: "售價", prop: "sell_price" }
-  // { label: "簡介", prop: "description", slot: "description" }
 ];
 async function init() {
   const { data } = await get({ collection: "products" });
-  console.log(data);
   products.value = data;
   loading.value = false;
 }
-function edit() {}
+function edit(id) {
+  router.push({ name: "EditProduct", params: { id } });
+}
 init();
 </script>
