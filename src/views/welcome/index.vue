73a4e040245a9a14a950e372e10944b339f675ed
orzorzooo <orzorzooo@gmail.com>
2023-05-26 21:25:57 +0800

    EDIT welcome index

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,9 +1,9 @@
+<template>
+  <h1>WELCOME TO ERP</h1>
+</template>
+
 <script setup lang="ts">
 defineOptions({
   name: "Welcome"
 });
 </script>
-
-<template>
-  <h1>Pure-Admin-Thin（非国际化版本）</h1>
-</template>
