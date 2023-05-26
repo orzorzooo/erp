1774acbe945a1cd1a0482772f3e3e1367cb6d647
orzorzooo <orzorzooo@gmail.com>
2023-05-26 16:55:50 +0800

    EDIT products create update路由

--- src/router/modules/products.ts
+++ src/router/modules/products.ts
@@ -9,15 +9,27 @@ export default {
       name: "Products",
       component: () => import("@/views/products/index.vue"),
       meta: {
-        title: "商品"
+        title: "所有商品"
       }
     },
     {
-      path: "/products/edit",
+      path: "/products/update/:id?",
       name: "EditProduct",
-      component: () => import("@/views/products/edit.vue"),
+      component: () => import("@/views/products/update.vue"),
+      props: true,
       meta: {
-        title: "新增"
+        title: "編輯",
+        showLink: false,
+        keepAlive: false
+      }
+    },
+    {
+      path: "/products/create",
+      name: "CreateProduct",
+      component: () => import("@/views/products/create.vue"),
+      props: true,
+      meta: {
+        title: "新增商品"
       }
     }
   ]
